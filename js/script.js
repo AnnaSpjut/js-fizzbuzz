e0b4de10bc0afd8b1277553d7d10bcc087136bc3
Anna Spjut <anna.spjut@gmail.com>
2024-02-23 13:27:13 +0400

    [add]console log

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 console.log('esercizio FizzBuzz')
 
+const list = document.getElementById('list')
+
 for(let i = 1; i <= 100; i++){
-  console.log(i);
-  if(i % 3 === 0){
-    
-  }
+  if(i % 15 === 0){
+    console.log('BuzzFizz');
+  }else if(i % 5 === 0){
+      console.log('Buzz');
+  }else if(i % 3 === 0){
+    console.log('Fizz');
+  }else(console.log(i))
 }
+   
+
+
+
+
+
+ 
